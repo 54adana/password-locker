f3ee6054ac8166e442af24f25059badf0ad0c9da
yvette <yvetteadan2000@gmail.com>
2019-11-11 17:02:58 +0300

    add creating a contact

--- run.py
+++ run.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python3.6
 from credentials import credentials
+def create_contact(fname,lname,password,email):
+    '''
+    Function to create a new credentials
+    '''
+    new_credentials = credentials(fname,lname,phone,email)
+    return new_credentials
