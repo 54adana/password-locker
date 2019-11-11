2a407c6c509b4c3675facd272bce48c76de78055
yvette <yvetteadan2000@gmail.com>
2019-11-11 17:13:32 +0300

    add delete credentials

--- run.py
+++ run.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3.6
 from credentials import credentials
-def create_contact(fname,lname,password,email):
+def create_credentials(fname,lname,password,email):
     '''
     Function to create a new credentials
     '''
-    new_credentials = credentials(fname,lname,phone,email)
+    new_credentials = credentials(fname,lname,password,email)
     return new_credentials
 def save_credentials(credentials):
     '''
     Function to save credentials
     '''
     contact.save_credentials()
+def del_credentials(credentials):
+    '''
+    Function to delete a credentials
+    '''
+    contact.delete_credentials()
